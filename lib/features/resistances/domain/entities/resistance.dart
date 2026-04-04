f8dc5b7204772850834a989dfe200935d12b3014
Nicolas <nicolasvera.dev@gmail.com>
2026-04-04 23:55:19 +0200

    feat: increase max description length for Command and Resistance entities
    - Updated maxDescriptionLength from 160 to 250 for both Command and Resistance classes to allow for more detailed descriptions, enhancing user input capabilities.

--- lib/features/resistances/domain/entities/resistance.dart
+++ lib/features/resistances/domain/entities/resistance.dart
@@ -4,7 +4,7 @@ import 'package:meta/meta.dart';
 @immutable
 class Resistance {
   static const int maxTitleLength = 60;
-  static const int maxDescriptionLength = 160;
+  static const int maxDescriptionLength = 250;
   static const String defaultEmoji = '🛡️';
   static const int maxTagsCount = 5;
   static const int maxTagLength = 20;
